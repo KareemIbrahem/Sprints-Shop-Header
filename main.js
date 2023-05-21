2768f806b22cba264a05d0330245c4201f81773a
Kareem Ibrahem <kareemibrahem1999@outlook.com>
2023-05-21 18:14:34 +0300

    last version

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#accountDropdownButton').click(function() {
+        $('#accountDropdownMenu').toggle();
+      });
+
+    $('#currencyDropdownButton').click(function() {
+        $('#currencyDropdownMenu').toggle();
+      });
+    
+      $('#languageDropdownButton').click(function() {
+        $('#languageDropdownMenu').toggle();
+      });
+
+      
+  });
+
+
+
+
+
+
+
+ 
